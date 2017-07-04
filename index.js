3e71a21502beababc2f6330f9dcbf0036f484603
Fahad Qazi <fahadqazi@gmail.com>
2017-07-04 23:12:22 +0100

    adding stop method

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var button = document.getElementById('button');
 var box = document.getElementById('box');
+var stop = document.getElementById('stop');
 
 var func = function(){
     box.classList.add('animate')
+}
+
+var fun1 = function(){
+    box.classList.remove('animate');
 }
