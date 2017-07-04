763549a64dacd27682e001e93458d1927c13b7b9
Fahad Qazi <fahadqazi@gmail.com>
2017-07-04 23:06:57 +0100

    refactor

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-console.log('hi');
 var button = document.getElementById('button');
-console.log(button);
 var box = document.getElementById('box');
-console.log(box);
 
 var func = function(){
-    console.log('what')
-    // console.log(box);
     box.classList.add('animate')
 }
